52fd91aabaa3d9ec511495d4a29d50430ac5197c
goforbg <techydevil@gmail.com>
2020-12-31 11:06:48 +0530

    Binds detail view, adds up button handling to activity and finishes part 12

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/details/DetailsFragment.kt
@@ -1,11 +1,79 @@
 package com.codinginflow.imagesearchapp.ui.details
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.codinginflow.imagesearchapp.R
+import com.codinginflow.imagesearchapp.databinding.FragmentDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment(R.layout.fragment_details) {
+
+    private val args by navArgs<DetailsFragmentArgs>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentDetailsBinding.bind(view)
+        binding.apply {
+            val photo = args.photo
+            //Loads Image
+            Glide.with(this@DetailsFragment)
+                .load(photo.urls.regular)
+                .error(R.drawable.ic_error)
+                .listener(object : RequestListener<Drawable> {
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        return false //Otherwise Glide will not load image
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        textViewCreator.isVisible = true
+                        textViewDescription.isVisible = photo.description != null
+                        return false //Otherwise Glide will not load image
+                    }
+
+                })
+                .into(imageView)
+            //Loads Description
+            textViewDescription.text = photo.description
+            //Links
+            val attributionUri = Uri.parse(photo.user.attributionUrl)
+            val attributionIntent = Intent(Intent.ACTION_VIEW, attributionUri)
+            //set Description and onclick of creator
+            textViewCreator.apply {
+                text = "Photo by ${photo.user.name} on Unsplash"
+                setOnClickListener {
+                    context.startActivity(attributionIntent)
+                }
+                paint.isUnderlineText = true
+            }
+        }
+
+    }
 
     companion object {
         private const val TAG = "DetailsFragment"
